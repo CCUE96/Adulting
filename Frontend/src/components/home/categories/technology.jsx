4ce357ffcb427a9dcf7f43f1732d09251e9badf5
Michael Freeman <mikef@Michaels-MacBook-Pro.local>
2024-08-05 22:05:19 -0400

    added functionality to click each category and see the questions inside them, also a go back button

---
+++ Frontend/src/components/home/categories/technology.jsx
@@ -0,0 +1,26 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import {Button} from '@mui/material'
+    
+const questions = ['Tech', 'Tech ', 'Question 3', 'Question 4'];
+
+export default function TechBox({goBack}) {
+    console.log('all categories rendered');
+    return (
+        <Box sx={{ display: 'flex', justifyContent: 'space-around', padding: 2, backgroundColor: 'rgba(255, 255, 255, 0.0)', backdropFilter: 'blur(10px)', color: 'white', width: '50vw', height: '30vh'}}>
+            <Box>
+                <Button onClick={goBack}>Back</Button>
+                <List>
+                    {questions.map((question, index) => (
+                        <Button sx={{color:'white'}} key={index} style={{ display: 'block', fontSize: 32}}>
+                            {question}
+                        </Button>
+                    ))}
+                </List>
+            </Box>
+        </Box>
+    );
+}
--- Frontend/src/components/home/maincategory.jsx
+++ Frontend/src/components/home/maincategory.jsx
@@ -1,8 +1,77 @@
 import SearchBar from "./searchbar";
 import AllCategories from "./categories/allcategories";
 import { Box } from '@mui/material';
+import FinanceBox from "./categories/finance";
+import { useEffect } from "react";
+import { useState } from "react";
+import HealthBox from "./categories/health";
+import TechBox from "./categories/technology";
+import EducationBox from "./categories/education";
+import HomeBox from "./categories/home";
+import OtherBox from "./categories/other";
+import SelfBox from "./categories/self";
+import NutritionBox from "./categories/nutrition";
 
 export default function MainCategory() {
+    const [showAll, setShowAll] = useState(true);
+    const [showFinance, setShowFinance] = useState(false);
+    const [showHealth, setShowHealth] = useState(false);
+    const [showTech, setShowTech] = useState(false)
+    const [showEduction, setShowEducation] = useState(false)
+    const [showHome, setShowHome]  = useState(false)
+    const [showSelf, setShowSelf]  = useState(false)
+    const [showNutrition, setShowNutrition]  = useState(false)
+    const [showOther, setShowOther]  = useState(false)
+
+    const handleFinanceClick = () => {
+        setShowAll(false);
+        setShowFinance(true);
+    };
+
+    const handleHealthClick = () => {
+        setShowAll(false)
+        setShowHealth(true)
+    }
+    const handleTechClick = () => {
+        setShowAll(false)
+        setShowTech(true)
+    }
+
+    const handleEducationClick = () => {
+        setShowEducation(true)
+        setShowAll(false)
+    }
+    const handleHomeClick = () => {
+        setShowAll(false)
+        setShowHome(true)
+    }
+
+    const handleSelfClick = () => {
+        setShowAll(false)
+        setShowSelf(true)
+    }
+    const handleNutritionClick = () => {
+        setShowAll(false)
+        setShowNutrition(true)
+    }
+    const handleOtherClick = () => {
+        setShowAll(false)
+        setShowOther(true)
+    }
+
+
+    const handleGoBack = () => {
+        setShowAll(true)
+        setShowFinance(false)
+        setShowHealth(false)
+        setShowTech(false)
+        setShowEducation(false)
+        setShowHome(false)
+        setShowOther(false)
+        setShowNutrition(false)
+        setShowSelf(false)
+    }
+
     return (
         <Box sx={{
             display: 'flex',
@@ -12,7 +81,15 @@ export default function MainCategory() {
             height: '100vh', // Full viewport height
         }}>
             <SearchBar />
-            <AllCategories />
+            {showAll && <AllCategories onFinanceClick={handleFinanceClick} onHealthClick ={handleHealthClick} onTechClick={handleTechClick} onEducationClick = {handleEducationClick} onHomeClick={handleHomeClick} onNutritionClick={handleNutritionClick} onSelfClick={handleSelfClick} onOtherClick={handleOtherClick}/>}
+            {showFinance && <FinanceBox goBack = {handleGoBack} />}
+            {showHealth && <HealthBox goBack = {handleGoBack} />}
+            {showTech && <TechBox goBack={handleGoBack}/>}
+            {showEduction && <EducationBox goBack={handleGoBack} />}
+            {showHome && <HomeBox goBack={handleGoBack} />}
+            {showSelf && <SelfBox goBack={handleGoBack} />}
+            {showNutrition && <NutritionBox goBack={handleGoBack} />}
+            {showOther && <OtherBox goBack={handleGoBack} />}
         </Box>
     );
 }
