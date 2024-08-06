782732768783e385dcb2f2bcae0ec75c0fa008b7
kaylommy <kaylajfreeman13@gmail.com>
2024-08-06 17:52:22 -0400

    moved folders into profile folder, added template for nav options  and functionality for profile navbar

---
+++ Frontend/src/components/profile/recipes/recipe.jsx
@@ -0,0 +1,26 @@
+
+import { Grid, Paper, Typography } from '@mui/material';
+
+const Recipes = () => {
+  return (
+    <Grid container spacing={2} sx={{ height: '65vh', marginTop: '1rem'}}>
+      <Grid item xs={12}>
+        <Paper sx={{ height: '20vh'}}>
+          <Typography sx={{ textAlign: 'center', padding: '1rem'}}><strong>Recipes</strong></Typography>
+        </Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper sx={{ height: '44vh'}}>
+          <Typography sx={{ textAlign: 'center', padding: '1rem'}}><strong>Monthly Spending</strong></Typography>
+          </Paper>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper sx={{ height: '44vh'}}>
+        <Typography sx={{ textAlign: 'center', padding: '1rem'}}><strong>Savings</strong></Typography>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Recipes;
