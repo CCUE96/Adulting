045cd871fc9951cbb0f5824217d47939ecb940e0
Christopher Cuellar <159393541+CCUE96@users.noreply.github.com>
2024-09-07 20:58:36 -0400

    Update connection.js

--- Backend/config/connection.js
+++ Backend/config/connection.js
@@ -1,22 +1,16 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const dbUri = process.env.DB_URI || 'mongodb://127.0.0.1:27017/Adulting';
-
-mongoose.connect(dbUri)
-  .then(() => {
-    console.log('Connected successfully to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-
 db.once('open', function() {
-  console.log('Connection to MongoDB is open');
+  console.log('Connected successfully');
 });
 
 module.exports = db;
